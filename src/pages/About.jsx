0aa4a3c17c0a685db773e3b2c6b717ae285a146c
Gaayathri M <79350586+gaayathrimuthuraman@users.noreply.github.com>
2023-06-01 11:41:18 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5bfc52cf8f3493a7f36210e0bff977f55466cc5e9393256d107f0e079050965b
-size 664
+import React from 'react'
+import Navbar from '../components/Navbar'
+import img1 from '../assets/img/AU1.png'
+import img2 from '../assets/img/AU2.png'
+import img3 from '../assets/img/AU3.png'
+import img4 from '../assets/img/AU4.png'
+import '../assets/css/Nav.css'
+
+
+const About = () => {
+    return (
+        <>
+        <Navbar />
+        <br />
+        <br /><br />
+        <img id='abimg' src={img1} alt="img" srcset="" />
+        <img id='abimg' src={img2} alt="img" srcset="" />
+        <img id='abimg' src={img3} alt="img" srcset="" />
+        <img id='abimg' src={img4} alt="img" srcset="" />
+        </>
+    )
+}
+
+
+export default About
