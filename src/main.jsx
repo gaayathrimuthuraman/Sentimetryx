8ce27e3cae3dc2a18b30f7af9c8186908227580f
Gaayathri M <79350586+gaayathrimuthuraman@users.noreply.github.com>
2023-06-01 11:31:54 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:364cdc00d0f1a919628cc19db941f1b50a049a0f187313622a4d0fbd1b9c0f6a
-size 301
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
