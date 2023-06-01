19031a351ca3c4576af415e8faac5285aef63186
Gaayathri M <79350586+gaayathrimuthuraman@users.noreply.github.com>
2023-06-01 11:42:28 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c5bc8a3871b1a822867b7ea211632b48fd18afa698156cdc5c3f342d2149bae3
-size 290
+import React from 'react'
+import Navbar from '../components/Navbar'
+import '../assets/css/home.css'
+import Main from '../components/Main'
+
+const Home = () => {
+    return (
+        <div id='bgcl'>
+        <Navbar />
+        <Main />
+        </div>
+    )
+}
+
+export default Home
